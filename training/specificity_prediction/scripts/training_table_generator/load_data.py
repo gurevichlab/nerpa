068eb024b38ad37fd8d541730da1d0d608147837
IlyaOlkhovsky <ilianolhin@gmail.com>
2024-08-22 17:24:05 +0200

    Refactored training table generation

---
+++ training/specificity_prediction/scripts/training_table_generator/load_data.py
@@ -0,0 +1,145 @@
+import argparse
+import yaml
+import pandas as pd
+from pathlib import Path
+from typing import (
+    Dict,
+    Iterable,
+    List,
+    Literal,
+    NamedTuple,
+    Optional,
+    Tuple,
+    Set,
+    Union
+)
+from collections import Counter, defaultdict, OrderedDict
+from src.monomer_names_helper import antiSMASH_MonomerName, MonomerResidue
+from src.generic.string import hamming_distance
+from dataclasses import dataclass
+
+
+def get_nerpa_supported_monomers(monomer_frequencies_tsv: Path,
+                                 num_monomers: Optional[int],
+                                 SEPARATOR: str) -> List[MonomerResidue]:
+    df = pd.read_csv(monomer_frequencies_tsv, sep=SEPARATOR)
+    selected_monomers = df['core'].head(num_monomers) if num_monomers is not None else df['core']
+    return sorted(selected_monomers)
+
+
+def get_short_name_to_nerpa_monomer_mapping(monomers_table_tsv: Path,
+                                            nerpa_supported_monomers: List[MonomerResidue],
+                                            nerpa_unknown_monomer: MonomerResidue,
+                                            SEPARATOR: str) -> Dict[antiSMASH_MonomerName, MonomerResidue]:
+    df = pd.read_csv(monomers_table_tsv, sep=SEPARATOR)
+
+    short_name_to_nerpa_monomer = defaultdict(lambda: nerpa_unknown_monomer)
+    for _, row in df.iterrows():
+        if row['core'] in nerpa_supported_monomers:
+            short_name_to_nerpa_monomer[row['as_short']] = row['core']
+
+    return short_name_to_nerpa_monomer
+
+
+def get_nerpa_monomer_to_SVM_short_names_mapping(as_short_names_svm: List[antiSMASH_MonomerName],
+                                                 short_name_to_nerpa_monomer: Dict[antiSMASH_MonomerName, MonomerResidue],
+                                                 nerpa_unknown_monomer: MonomerResidue) -> Dict[MonomerResidue, List[antiSMASH_MonomerName]]:
+    nerpa_monomer_to_SVM_short_names: Dict[MonomerResidue, List[antiSMASH_MonomerName]] = defaultdict(list)
+
+    # Populate the dictionary
+    for as_short_name in as_short_names_svm:
+        # Map the SVM short name to the corresponding nerpa monomer
+        nerpa_monomer = short_name_to_nerpa_monomer.get(as_short_name, nerpa_unknown_monomer)
+        if nerpa_monomer != nerpa_unknown_monomer:  # !! see below
+            nerpa_monomer_to_SVM_short_names[nerpa_monomer].append(as_short_name)
+
+    # !! Think about me !!
+    # !! Some SVM-supported monomers are rare (e.g. 'Pgl' is present only in 3 A-domains in the signatures file).
+    # !! As the result, they could be not in the Nerpa core monomers and be converted to "Unknown" (special_monomer_name)
+    # !! Without the "if" check above, this special monomer will map to only these few (e.g., 1) SVM rare monomers.
+    # !! This could bias the downstream results -- we will treat this as if SVM predict some scores for all "Unknown"s,
+    # !! not just a few supported by SVM and ignored by Nerpa. Let's be safe and just ignore SVM predictions for them.
+
+    return nerpa_monomer_to_SVM_short_names
+
+
+AA10_code = str
+AA34_code = str
+AA_code = Union[AA10_code, AA34_code]
+SubstrateCodesDict = Dict[MonomerResidue, Dict[AA_code, int]]
+
+
+def read_substrate_to_aa_codes(extended_signatures_tsv: Path,
+                               short_name_to_nerpa_monomer: Dict[antiSMASH_MonomerName, MonomerResidue],
+                               SEPARATOR: str) \
+        -> Tuple[SubstrateCodesDict, SubstrateCodesDict]:
+    df = pd.read_csv(extended_signatures_tsv, sep=SEPARATOR)
+
+    # Initialize the defaultdicts for aa10 and aa34 codes
+    substrate_to_aa10_codes = defaultdict(Counter)
+    substrate_to_aa34_codes = defaultdict(Counter)
+
+    # Iterate over each row in the DataFrame
+    for _, row in df.iterrows():
+        substrates: List[antiSMASH_MonomerName] = eval(row['substrates'])  # Evaluates the list from string to Python list
+
+        nerpa_monomers = [short_name_to_nerpa_monomer[substrate] for substrate in substrates]
+
+        aa10_code = row['aa10']
+        aa34_code = row['aa34']
+
+        num_domains = row['num_domains']
+
+        # Update the dictionaries for each nerpa_monomer
+        for nerpa_monomer in nerpa_monomers:
+            substrate_to_aa10_codes[nerpa_monomer][aa10_code] += num_domains
+            substrate_to_aa34_codes[nerpa_monomer][aa34_code] += num_domains
+
+    return substrate_to_aa10_codes, substrate_to_aa34_codes
+
+
+@dataclass
+class TrainingTableInputData:
+    nerpa_supported_monomers: List[MonomerResidue]
+    nerpa_unknown_monomer: MonomerResidue
+    short_name_to_nerpa_monomer: Dict[antiSMASH_MonomerName, MonomerResidue]
+    nerpa_monomer_to_SVM_short_names: Dict[MonomerResidue, List[antiSMASH_MonomerName]]
+    substrate_to_aa10_codes: SubstrateCodesDict
+    substrate_to_aa34_codes: SubstrateCodesDict
+    extended_signatures_df: pd.DataFrame
+
+
+def load_data(monomer_frequencies_tsv: Path,
+              monomers_table_tsv: Path,
+              extended_signatures_tsv: Path,
+              num_monomers: Optional[int],
+              nerpa_unknown_monomer: MonomerResidue,
+              SEPARATOR: str) -> TrainingTableInputData:
+    # Get the list of supported monomers
+    nerpa_supported_monomers = get_nerpa_supported_monomers(monomer_frequencies_tsv, num_monomers, SEPARATOR)
+
+    # Get the mapping from short names to Nerpa monomers
+    short_name_to_nerpa_monomer = get_short_name_to_nerpa_monomer_mapping(monomers_table_tsv,
+                                                                          nerpa_supported_monomers,
+                                                                          nerpa_unknown_monomer,
+                                                                          SEPARATOR)
+
+    # Get the mapping from Nerpa monomers to SVM short names
+    extended_signatures_df = pd.read_csv(extended_signatures_tsv, sep=SEPARATOR)
+    as_short_names_svm = list(extended_signatures_df.columns[4:])
+    nerpa_monomer_to_SVM_short_names = get_nerpa_monomer_to_SVM_short_names_mapping(as_short_names_svm,
+                                                                                    short_name_to_nerpa_monomer,
+                                                                                    nerpa_unknown_monomer)
+
+    # Get the dictionaries for aa10 and aa34 codes
+    substrate_to_aa10_codes, substrate_to_aa34_codes = read_substrate_to_aa_codes(extended_signatures_tsv,
+                                                                                  short_name_to_nerpa_monomer,
+                                                                                  SEPARATOR)
+
+    return TrainingTableInputData(nerpa_supported_monomers,
+                                  nerpa_unknown_monomer,
+                                  short_name_to_nerpa_monomer,
+                                  nerpa_monomer_to_SVM_short_names,
+                                  substrate_to_aa10_codes,
+                                  substrate_to_aa34_codes,
+                                  pd.read_csv(extended_signatures_tsv, sep=SEPARATOR))
---
+++ training/specificity_prediction/scripts/training_table_generator/main.py
@@ -0,0 +1,80 @@
+import argparse
+from pathlib import Path
+from typing import List
+from src.monomer_names_helper import MonomerResidue
+from training.specificity_prediction.scripts.training_table_generator.load_data import load_data
+from training.specificity_prediction.scripts.training_table_generator.process_data import generate_nerpa_table_entries
+from training.specificity_prediction.scripts.training_table_generator.write_data import write_monomer_signatures, write_nerpa_table
+
+SEPARATOR = "\t"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Process monomer signature data.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    # Input group
+    input_group = parser.add_argument_group("input", "Input options")
+    input_group.add_argument("--num-monomers", type=int, default=None,
+                             help="Number of most frequent monomers to consider. By default all monomers are included.")
+    input_group.add_argument("--unknown-monomer-name", type=str, default="Unknown",
+                             help="Special name designating all rare (unsupported) monomers.")
+    input_group.add_argument("--monomer-frequencies", type=str, default="data/core_frequency.tsv",
+                             help="Path to a TSV file with all core monomers and their frequencies sorted from the most common to the rarest.")
+    input_group.add_argument("--extended-signatures", type=str, default="data/extended_signatures.tsv",
+                             help="Path to the extended signatures file.")
+    input_group.add_argument("--monomers-table", type=str, default="data/monomers_unique.tsv",
+                             help="Path to the monomer names conversion table.")
+
+    # Output group
+    output_group = parser.add_argument_group("output", "Output options")
+    output_group.add_argument("--monomer-signatures-dict", type=str, default="data/monomer_signatures.yaml",
+                              help="Path to a YAML file to store the dictionary with all aa10 and aa34 signatures per every supported core monomer.")
+    output_group.add_argument("--nerpa-training-table", type=str, default="data/nerpa_scoring_table.tsv",
+                              help="Path to a TSV file to store the main specificity training table.")
+
+    return parser.parse_args()
+
+
+def ensure_output_directories_exist(output_paths: List[Path]):
+    for path in output_paths:
+        # Convert the file path to a Path object
+        parent_dir = Path(path).parent
+
+        # Create the directory (and any missing parents) if it doesn't exist
+        if not parent_dir.exists():
+            parent_dir.mkdir(parents=True, exist_ok=True)
+            print(f"Created directory: {parent_dir}")
+        # else:
+        #     print(f"Directory already exists: {parent_dir}")
+
+
+def main():
+    # Parse arguments
+    args = parse_args()
+
+    # reading
+    input_data = load_data(monomer_frequencies_tsv=args.monomer_frequencies,
+                           monomers_table_tsv=args.monomers_table,
+                           extended_signatures_tsv=args.extended_signatures,
+                           num_monomers=args.num_monomers,
+                           nerpa_unknown_monomer=MonomerResidue(args.unknown_monomer_name),
+                           SEPARATOR=SEPARATOR)
+
+    # processing
+    nerpa_table_entries = generate_nerpa_table_entries(input_data)
+
+    # writing
+    ensure_output_directories_exist([args.monomer_signatures_dict, args.nerpa_training_table])
+    write_monomer_signatures(args.monomer_signatures_dict,
+                             input_data.substrate_to_aa10_codes,
+                             input_data.substrate_to_aa34_codes,
+                             input_data.nerpa_supported_monomers,
+                             input_data.nerpa_unknown_monomer)
+    write_nerpa_table(args.nerpa_training_table, nerpa_table_entries, SEPARATOR)
+
+
+if __name__ == "__main__":
+    main()
