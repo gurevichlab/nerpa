dc4504b81f486714f6718292a3ca5608b98a1421
IlyaOlkhovsky <ilianolhin@gmail.com>
2024-09-04 18:00:05 +0200

    Updated calculate norine frequencies script

--- src/scripts/calculate_norine_monomers_freqs.py
+++ src/scripts/calculate_norine_monomers_freqs.py
@@ -3,13 +3,17 @@ from pathlib import Path
 import csv
 from collections import Counter
 import pandas as pd
+from pathlib import Path
 
-monomer_names_table = pd.read_csv(Path('/home/ilianolhin/git/nerpa2/configs/monomers_tables_v3/monomers_unique.tsv'), sep='\t')
+# this script is in nerpa_dir/src/scripts/calculate_norine_monomers_freqs.py
+nerpa_dir = Path(__file__).parent.parent.parent
+monomer_names_table = pd.read_csv(nerpa_dir / 'configs' / 'monomer_names_table.tsv',
+                                  sep='\t')
 monomer_names_helper = MonomerNamesHelper(monomer_names_table)
 
-norine_table = pd.read_csv(Path('/home/ilianolhin/git/nerpa2/configs/norineText.csv'), sep=';')
+norine_table = pd.read_csv(nerpa_dir / 'data/norine_graphs.csv', sep=';')
 # q: set column names to ['norine_id', 'norine_familty', 'graph_representation']
-norine_table.columns = ['norine_id', 'norine_familty', 'graph_representation']
+norine_table.columns = ['norine_id',  'graph_representation']
 norine_table = norine_table.replace({float('nan'): None})
 
 norine_monomers = []
@@ -25,6 +29,7 @@ norine_residues = [monomer_names_helper.parsed_name(monomer_name, 'norine').resi
 residues_counter = Counter(norine_residues)
 # q: write residues frequencies fractions
 residues_freqs = {residue: count / len(norine_residues) for residue, count in residues_counter.most_common()}
-with open('/home/ilianolhin/git/nerpa2/configs/norine_residues_freqs.txt', 'w') as file:
-    s = repr(residues_freqs).replace("'","")
-    file.write(s)
+with open(nerpa_dir / 'data/norine_residues_freqs.tsv', 'w') as file:
+    file.write('residue\tfrequency\n')
+    for residue, freq in residues_freqs.items():
+        file.write(f'{residue}\t{freq}\n')
