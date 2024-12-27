0a06b5c02330e63e54e8cf3e017722f10b0761b3
Alexey Gurevich <alexeigurevich@gmail.com>
2024-12-27 17:52:41 +0100

    HTML report: embedded detailed match view (part 3: get rid of the old per-match HTML reports)

--- src/build_output/html_reporter.py
+++ src/build_output/html_reporter.py
@@ -33,16 +33,10 @@ def _apply_substitutions(template: str, substitutions: Dict[str, str]) -> str:
 def create_html_report(config_paths: ConfigPaths, matches: List[Match]):
     current_dir = Path(__file__).resolve().parent
     template_main_report_path = current_dir / 'main_report_template.html'
-    template_match_report_path = current_dir / 'match_report_template.html'
     main_report_path = config_paths.html_report
 
     with open(template_main_report_path, 'r') as file:
         main_report_html_template = file.read()
-
-    # TODO/FIXME: It is better (but more difficult) to load everything in just one main HTML
-    #  and skip the creation of zillions per match JSONs and HTML reports
-    with open(template_match_report_path, 'r') as file:
-        match_report_html_template = file.read()
 
     # TODO: Maybe add these paths directly to config_paths?
     html_aux_dir = config_paths.main_out_dir / 'html_aux'
@@ -63,24 +57,3 @@ def create_html_report(config_paths: ConfigPaths, matches: List[Match]):
         file.write(main_html_report)
     # copying logo to be embedded in the HTML report
     shutil.copy(config_paths.logo, html_aux_dir)
-
-    # per-match detailed HTML reports and associated JSONs
-    # TODO?: limit the number of generate reports to the first (best) X matches? e.g., 1000
-    for match_dict in match_dicts:
-        match_data_js_path = html_aux_dir / f'match_{match_dict["Match_ID"]}.js'
-        match_html_report_path = html_aux_dir / f'match_{match_dict["Match_ID"]}.html'
-        with open(match_data_js_path, 'w') as json_file:
-            json_file.write('var data = ')
-            json.dump(match_dict, json_file, indent=4)
-
-        path_substitutions = {
-            # Note: the first one could be done with Path's .relative_to()
-            # but it will fail for the rest two -- it can't properly form the "../" like output
-            '{{DATA_JSON_PATH}}': os.path.relpath(match_data_js_path, start=html_aux_dir),
-            '{{MAIN_REPORT_PATH}}': os.path.relpath(main_report_path, start=html_aux_dir),
-            '{{ANTISMASH_OUT_DIR}}': os.path.relpath(config_paths.antismash_out_dir, start=html_aux_dir),
-            '{{NRP_IMAGES_DIR}}': os.path.relpath(config_paths.nrp_images_dir, start=html_aux_dir)
-        }
-        match_html_report = _apply_substitutions(match_report_html_template, path_substitutions)
-        with open(match_html_report_path, 'w') as file:
-            file.write(match_html_report)
