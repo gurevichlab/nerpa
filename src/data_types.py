a2155f80e2e419176a35e7abf4fcd24a7295e553
IlyaOlkhovsky <ilianolhin@gmail.com>
2025-03-04 16:00:56 +0100

    Parsing paras results...

--- src/data_types.py
+++ src/data_types.py
@@ -29,6 +29,7 @@ Logger = Any  # some magical logger used throughout the pipeline
 SMILES = str
 AA34 = str  # 34-letter code
 AA10 = str  # 10-letter Stachelhaus code
+Prob = float
 LogProb = float
 ResidueScores = Dict[MonomerResidue, LogProb]
 
