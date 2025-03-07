e7cdfe2ef9d18f5d0f2296f886977be362875d82
IlyaOlkhovsky <ilianolhin@gmail.com>
2025-03-07 13:50:07 +0100

    Updated checkpoints_heuristic

--- src/matching/hmm_checkpoints_heuristic.py
+++ src/matching/hmm_checkpoints_heuristic.py
@@ -1,4 +1,5 @@
 from typing import (
+    Callable,
     Dict,
     List,
     TYPE_CHECKING,
@@ -22,13 +23,25 @@ def nrp_monomer_matches_prediction(nrp_monomer: rBAN_Monomer,
     return nrp_monomer.residue == predicted_residues[0]
 
 
+def _get_checkpoints(len_bgc_modules: int,
+                     len_nrp_monomers: int,
+                     are_equal: Callable[[int, int], bool]) -> List[Tuple[int, int]]:  # (i, j) means i-th module corresponds to j-th monomer
+    return []
+
+
 def get_checkpoints(hmm: 'DetailedHMM',
                     nrp_monomers: List[rBAN_Monomer]) -> List[Tuple[StateIdx, int]]:
     bgc_modules_predictions = [hmm.hmm_helper.get_emissions(module, hmm.bgc_variant.has_pks_domains())
                                for module in hmm.bgc_variant.modules]
 
-    # ... (find matching module-monomer pairs here) ...
-    matched_pairs = []  # a placeholder
+    def are_equal(module_idx: int,
+                    monomer_idx: int) -> bool:
+        return nrp_monomer_matches_prediction(nrp_monomers[monomer_idx],
+                                              bgc_modules_predictions[module_idx])
+
+    matched_pairs = _get_checkpoints(len(hmm.bgc_variant.modules),
+                                     len(nrp_monomers),
+                                     are_equal)
 
     inner_checkpoints = [(hmm._module_idx_to_match_state_idx[module_idx], monomer_idx)
                          for module_idx, monomer_idx in matched_pairs]
