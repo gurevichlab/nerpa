5f65295b03fe4051d5de13eead88b8889a18c769
IlyaOlkhovsky <ilianolhin@gmail.com>
2025-03-08 00:28:35 +0100

    Moved aa10 and aa34 codes from monomer_names_helper to cfg

--- src/monomer_names_helper.py
+++ src/monomer_names_helper.py
@@ -63,30 +63,16 @@ PKS_MONOMER = NRP_Monomer(residue=PKS,
 class MonomerNamesHelper:
     names_table: pd.DataFrame
     supported_residues: List[MonomerResidue]
-    _known_aa10_codes_as: Dict[antiSMASH_MonomerName, List[AA10]]
-    _known_aa34_codes_as: Dict[antiSMASH_MonomerName, List[AA34]]
     _cache: Dict[Tuple[str, str], NRP_Monomer] = None
 
     mon_to_int: Dict[NRP_Monomer, MonCode] = None
     int_to_mon: Dict[MonCode, NRP_Monomer] = None
     pks_names: List[NorineMonomerName] = None
-    known_aa10_codes: Dict[MonomerResidue, List[AA10]] = None
-    known_aa34_codes: Dict[MonomerResidue, List[AA34]] = None
 
     def __post_init__(self):
         self._cache = {}
         if self.pks_names is None:
             self.pks_names = []
-
-        self.known_aa10_codes = defaultdict(list)
-        for as_mon_name, aa10_codes in self._known_aa10_codes_as.items():
-            mon = self.parsed_name(as_mon_name, 'antismash')
-            self.known_aa10_codes[mon.residue].extend(aa10_codes)
-
-        self.known_aa34_codes = defaultdict(list)
-        for as_mon_name, aa34_codes in self._known_aa34_codes_as.items():
-            mon = self.parsed_name(as_mon_name, 'antismash')
-            self.known_aa34_codes[mon.residue].extend(aa34_codes)
 
         self.int_to_mon = {}
         self.mon_to_int = {}
