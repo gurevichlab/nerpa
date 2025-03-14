ae3f33d477bf13329bea33efd7b96addec8a24a4
Ilia Olkhovskii <100956858+IlyaOlkhovsky@users.noreply.github.com>
2025-03-14 16:33:36 +0100

    Update combinatorics.py

--- src/generic/combinatorics.py
+++ src/generic/combinatorics.py
@@ -169,10 +169,10 @@ def longest_increasing_subsequence(seqs: List[T]) -> list[int]:
     # returns list of indexes
     d: Dict[int, T] = {}
     # 
-edges: List[Tuple[int, int]] = [(1, 4), (1, 2), (1, 1), (4, 2)] 
-1. Sort edges: [(1, 2), (2, 1), (3, 4), (4, 2)]
-2. Make the list of 2nd elements: snd_elements = [4, 2, 1, 2]
-3. Extract LIS from snd_elements lis <- [2, 4]  # indexes [0, 2]
+edges: List[Tuple[int, int]] = [(4, 2), (1, 4), (1, 1), (1, 2), (2, 1), (3, 4)] 
+1. Sort edges first by increasing first item and then by decreasing second item: [(1, 4), (1, 2), (1, 1), (2, 1), (3, 4), (4, 2)]
+2. Make the list of 2nd elements: snd_elements = [4, 2, 1, 1, 4, 2]
+3. Extract LIS from snd_elements lis <- [2, 4]  # indexes [1, 4]
 4. Take pairs with corresponding indexes
 '''
 def longest_increasing_subsequence(pairs: list[tuple[int, int]]) -> list[int]:
