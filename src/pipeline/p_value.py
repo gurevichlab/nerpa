017df14f55344f0c597e9438879b5c49ddd56166
ppaa23 <peterpopov1999@gmail.com>
2025-03-31 15:54:48 +0300

    class PValueEstimator added, main function changed to calculated p-values for all possible discrete thresholds at once

--- src/pipeline/p_value.py
+++ src/pipeline/p_value.py
@@ -5,7 +5,7 @@ import numpy as np
 StateIdx = NewType('StateIdx', int)
 LogProb = NewType('LogProb', float | float('-inf'))
 DiscreteProb = NewType('DiscreteProb', int)
-Prob = NewType('Prob', float) # actual probability value in range [0, 1]
+Prob = NewType('Prob', float)  # actual probability value in range [0, 1]
 
 # DiscreteProb is a value between MIN_DISCRETE_VALUE and MAX_DISCRETE_PROB
 MAX_DISCRETE_PROB = DiscreteProb(1000)
@@ -17,8 +17,19 @@ LOG_PROB_MAX_VALUE = LogProb(0)
 
 
 class HMM(NamedTuple):
-    transitions: List[List[Tuple[StateIdx, LogProb]]]  # u -> [(v, log_prob(u -> v))]
+    transitions: List[List[Tuple[StateIdx, LogProb]]]   # u -> [(v, log_prob(u -> v))]
     emissions: List[List[LogProb]]  # u -> [log_prob(u -> emission)]
+
+
+class PValueEstimator:
+    def __init__(self, hmm: HMM):
+        self.hmm = hmm
+        self.p_values = compute_hmm_p_values(hmm)
+
+    def __call__(self, threshold_score: LogProb) -> Prob:
+        disc_threshold_score = to_discrete_prob(threshold_score)
+        p_value = self.p_values[disc_threshold_score]
+        return p_value
 
 
 def to_discrete_prob(prob: LogProb) -> DiscreteProb:
@@ -37,32 +48,24 @@ def to_prob(disc_prob: DiscreteProb) -> LogProb:
         return float('-inf')
     else:
         # Map [MIN_DISCRETE_PROB, MAX_DISCRETE_PROB] to [LOG_PROB_MIN_VALUE, LOG_PROB_MAX_VALUE]
-        prob = (LOG_PROB_MIN_VALUE + (disc_prob - MIN_DISCRETE_PROB) /
+        log_prob = (LOG_PROB_MIN_VALUE + (disc_prob - MIN_DISCRETE_PROB) /
                        (MAX_DISCRETE_PROB - MIN_DISCRETE_PROB) * (LOG_PROB_MAX_VALUE - LOG_PROB_MIN_VALUE))
-        return LogProb(prob)
+        return LogProb(log_prob)
 
 
-# TODO: apply for the list of all possible thresholds
-def compute_p_value(hmm: HMM, threshold_score: LogProb) -> Prob:
-    """
-    Computes p-value of given HMM with respect to the given threshold score,
-    that is a probability of finding a path with a score not lesser than the threshold score.
-    The path corresponding to the threshold score is not counted.
-    """
-    disc_threshold_prob = to_discrete_prob(threshold_score)
-
+def compute_hmm_p_values(hmm: HMM) -> np.ndarray:
     num_states = len(hmm.transitions)
     initial_state = 0
     final_state = num_states - 1
 
     # dp[state, discrete_prob] = number of paths ending at the state 'state' and
     # having discretized probability 'discrete_prob'
-    dp = np.zeros((num_states, MAX_DISCRETE_PROB + 1), dtype=DiscreteProb)
+    dp = np.zeros((num_states, MAX_DISCRETE_PROB + 1), dtype=int)
 
     dp[initial_state][MAX_DISCRETE_PROB] = 1
 
     # Iterate over probabilities first, from highest to lowest
-    for disc_prob in range(MAX_DISCRETE_PROB, disc_threshold_prob - 1, -1):
+    for disc_prob in range(MAX_DISCRETE_PROB, MIN_DISCRETE_PROB - 1, -1):
         # Then iterate over states
         for state in range(num_states):
             # Skip if no paths with this probability for this state
@@ -70,7 +73,7 @@ def compute_p_value(hmm: HMM, threshold_score: LogProb) -> Prob:
                 continue
 
             number_of_paths = dp[state, disc_prob]
-            path_prob = to_prob(disc_prob)
+            path_prob = to_prob(DiscreteProb(disc_prob))
 
             # Process each outgoing transition
             for next_state, transition_prob in hmm.transitions[state]:
@@ -87,15 +90,20 @@ def compute_p_value(hmm: HMM, threshold_score: LogProb) -> Prob:
 
                     dp[next_state][disc_new_path_prob] += number_of_paths
 
-    p_value = 0
-    for disc_prob in range(disc_threshold_prob, MAX_DISCRETE_PROB + 1):
-        number_of_paths = dp[final_state, disc_prob]
-        if number_of_paths > 0:
-            # Yield p-value in range [0, 1]
-            actual_prob = math.exp(to_prob(disc_prob))
-            p_value += actual_prob * number_of_paths
+    p_values = np.zeros((MAX_DISCRETE_PROB - MIN_DISCRETE_PROB + 1), dtype=float)
 
-    p_value -= math.exp(threshold_score) # to not count the contribution of the found path itself
-    p_value = max(p_value, 0) # to prevent possible miscalculation caused by discretization
+    for k in range(MIN_DISCRETE_PROB, MAX_DISCRETE_PROB + 1):
+        p_value = 0.0
+        for disc_prob in range(k, MAX_DISCRETE_PROB + 1):
+            actual_prob = math.exp(to_prob(DiscreteProb(disc_prob)))
+            p_value += actual_prob * dp[final_state, disc_prob]
 
-    return p_value
+        # Not count the contribution of the found path itself
+        p_value -= math.exp(to_prob(DiscreteProb(k)))
+
+        # Prevent possible miscalculation caused by discretization
+        p_value = min(max(p_value, 0.0), 1.0)
+
+        p_values[k] = Prob(p_value)
+
+    return p_values
