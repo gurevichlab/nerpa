dc8f506442a2c9b02df7161bc62a9758b6415c85
IlyaOlkhovsky <ilianolhin@gmail.com>
2025-09-05 22:38:59 +0200

    Updated default module meth/epi freqs

--- nerpa.py
+++ nerpa.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
+from typing import List
 
+from src.data_types import BGC_Variant, BGC_Module_Modification
 from src.monomer_names_helper import MonomerResidue
 from src.pipeline.pipeline_helper import PipelineHelper
 from src.pipeline.logger import NerpaLogger
@@ -10,11 +12,12 @@ from src.write_results import write_bgc_variants
 from pathlib import Path
 
 
+
 def main(log: NerpaLogger):  # log is passed as an argument to make it easier to write log in case of exception
     pipeline_helper = PipelineHelper(log)
 
     bgc_variants = pipeline_helper.get_bgc_variants()
-    #bgc_variants[2].modules[1].residue_score['unknown'] = 5.0
+    #compute_modification_freqs(bgc_variants)
     hmms = pipeline_helper.construct_hmms(bgc_variants)
 
     nrp_variants, rban_records = pipeline_helper.get_nrp_variants_and_rban_records()
@@ -36,3 +39,29 @@ if __name__ == "__main__":
     finally:
         # TODO: clean up: remove all intermediate files
         pass
+
+'''
+def compute_modification_freqs(bgc_variants: List[BGC_Variant]):
+    processed_bgc_ids = set()
+
+    total_modules = 0
+    total_methylated = 0
+    total_epimerized = 0
+    for bgc_variant in bgc_variants:
+        if bgc_variant.bgc_variant_id.bgc_id in processed_bgc_ids:
+            continue
+        processed_bgc_ids.add(bgc_variant.bgc_variant_id.bgc_id)
+
+        for module in bgc_variant.modules:
+            total_modules += 1
+            if BGC_Module_Modification.METHYLATION in module.modifications:
+                total_methylated += 1
+            if BGC_Module_Modification.EPIMERIZATION in module.modifications:
+                total_epimerized += 1
+
+    methylation_freq = total_methylated / total_modules
+    epimerization_freq = total_epimerized / total_modules
+    print(f"Total modules: {total_modules}")
+    print(f"Methylation frequency: {methylation_freq:.4f}")
+    print(f"Epimerization frequency: {epimerization_freq:.4f}")
+'''
