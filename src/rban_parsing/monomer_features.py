f19b255592c32e447ab3049ecb2287fc5e71baac
IlyaOlkhovsky <ilianolhin@gmail.com>
2025-10-06 18:55:30 +0200

    Fixing bugs with monomer names helper

--- src/rban_parsing/monomer_features.py
+++ src/rban_parsing/monomer_features.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 from enum import Enum, auto
 from src.generic.graphs import BackboneSequence
 from src.rban_parsing.rban_parser import MonomerInfo
-from src.monomer_names_helper import UNKNOWN_RESIDUE, MonomerNamesHelper
+from src.monomer_names_helper import UNKNOWN_RESIDUE, MonomerNamesHelper, PKS_RESIDUE
 from src.rban_parsing.monomer_features_types import MonomerFeature, MonomerFeatures
 
 
@@ -17,8 +17,8 @@ def get_monomer_features(monomer_idx: int,
     pairs = [
         (MonomerFeature.START_OF_FRAGMENT, monomer_pos == 0),
         (MonomerFeature.END_OF_FRAGMENT, monomer_pos == len(backbone_sequence) - 1),
-        (MonomerFeature.UNKNOWN_RESIDUE, names_helper.parsed_name(monomer_info.name, 'norine').residue == UNKNOWN_RESIDUE),
+        (MonomerFeature.UNKNOWN_RESIDUE, names_helper.parsed_name(monomer_info.name, 'rBAN/Norine').residue == UNKNOWN_RESIDUE),
         (MonomerFeature.PKS_HYBRID, monomer_info.is_pks_hybrid),
-        (MonomerFeature.PKS, monomer_info.name in names_helper.pks_names)
+        (MonomerFeature.PKS, names_helper.parsed_name(monomer_info.name, 'rBAN/Norine').residue == PKS_RESIDUE)
     ]
     return tuple(feature for feature, is_present in pairs if is_present)
