10f0d05113883969472875412f86e2cda4191598
IlyaOlkhovsky <ilianolhin@gmail.com>
2026-01-27 19:36:27 +0100

    (  •̀⤙•́  )

--- src/benchmarking/plots_data_helper.py
+++ src/benchmarking/plots_data_helper.py
@@ -6,7 +6,6 @@ from typing import Literal, Dict, Sequence, List, Optional, Set, Tuple
 import polars as pl
 from collections import defaultdict
 
-from scripts.build_mibig_info_table.create_final_table import nrp_id_to_bgc_id
 from src.benchmarking.data_frames import MIBiG_BGCs_Info, PNRPDB_Info, PNRPDB_Compound_Similarity
 from src.benchmarking.data_helper_external_methods import (
     compute_num_correct_matches, compute_score_correctness,
