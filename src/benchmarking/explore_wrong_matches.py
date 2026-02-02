2f7ccfe42e84dd3cc983f5d35a2c71b79f479a11
IlyaOlkhovsky <ilianolhin@gmail.com>
2026-02-02 12:53:01 +0100

    Changed comparison method in explore_wrong_matches

--- src/benchmarking/explore_wrong_matches.py
+++ src/benchmarking/explore_wrong_matches.py
@@ -1,5 +1,6 @@
 from src.benchmarking.plots_data_helper import PlotsDataHelper
 from src.benchmarking.nerpa_report import NerpaReport
+from src.benchmarking.data_frames import PNRPDB_Compound_Similarity
 from pathlib import Path
 import polars as pl
 
@@ -15,15 +16,29 @@ def main():
                                                   score_column='LogOdds_vs_avg_BGC')
     print(nerpa2_report.head())
 
-    incorrect_matches = (
-        nerpa2_report.filter(pl.col(NerpaReport.IS_CORRECT) == False)
-        .select([NerpaReport.SCORE,
-                              NerpaReport.BGC_ID,
-                              NerpaReport.NRP_ID,
-                              NerpaReport.MATCH_RANK,
-                              NerpaReport.MATCH_RANK_FOR_BGC,
-                              NerpaReport.MATCH_RANK_FOR_NRP,])
+    nerpa2_report = nerpa2_report.with_columns(
+        pl.struct([NerpaReport.BGC_ID, NerpaReport.NRP_ID])
+        .map_elements(lambda s: data_helper.match_is_correct(
+            bgc_id=s[NerpaReport.BGC_ID],
+            nrp_iso_class=s[NerpaReport.NRP_ID],
+            cmp_mode=PNRPDB_Compound_Similarity.NERPA_NO_MORE_ONE_SUB_ALLOW_UNK_CHR,
+        ))
+        .alias("is_correct_tmp")
     )
+
+    incorrect_matches = (
+        nerpa2_report
+        .filter(pl.col("is_correct_tmp") == False)
+        .select([
+            NerpaReport.SCORE,
+            NerpaReport.BGC_ID,
+            NerpaReport.NRP_ID,
+            NerpaReport.MATCH_RANK,
+            NerpaReport.MATCH_RANK_FOR_BGC,
+            NerpaReport.MATCH_RANK_FOR_NRP,
+        ])
+    )
+
     # q: write incorrect matches to tsv
     incorrect_matches.write_csv('incorrect_matches.tsv', separator='\t')
 
