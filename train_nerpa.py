587e1a448c1ef9b653c7b98ddb8d1543ac2bdfa9
ilianolhin <ilianolhin@gmail.com>
2026-02-10 17:11:19 +0100

    ¯\_(ツ)_/¯

--- train_nerpa.py
+++ train_nerpa.py
@@ -91,7 +91,7 @@ def load_command_line_args(nerpa_dir: Path) -> CommandlineArgs:
                                                  "based on Nerpa results on approved matches")
     parser.add_argument("--approved-matches", type=Path,
                         default=nerpa_dir / 'data/for_training_and_testing/approved_matches.yaml')
-    #parser.add_argument("--bgc-variants", type=Path)
+    parser.add_argument("--bgc-variants", type=Path)
 
     parser.add_argument("--output-dir", type=Path,
                         default=nerpa_dir / 'training_results')
